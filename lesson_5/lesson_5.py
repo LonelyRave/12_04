cc79496abd3c57fdfaf3521aae1e804c32ed4777
jenya <132168049+LonelyRave@users.noreply.github.com>
2023-05-03 16:55:42 +0100

    uncommented the code

--- lesson_5/lesson_5.py
+++ lesson_5/lesson_5.py
@@ -2,39 +2,41 @@
 # якщо дискримінант від'ємний викликати виняток DiscriminantError і вивести відповідне повідомлення.
 
 
-# import math
-#
-# class DiscriminantError(Exception):
-#     pass
-#
-# a = float(input("Enter the coefficient a: "))
-# b = float(input("Enter the coefficient b: "))
-# c = float(input("Enter the coefficient c: "))
-#
-# try:
-#     discriminant = b**2 - 4*a*c
-#     if discriminant < 0:
-#         raise DiscriminantError("Discriminant is negative, cannot take square root of negative number.")
-#     elif discriminant == 0:
-#         x = -b / (2*a)
-#         print("The equation has one real root: x =", x)
-#     else:
-#         x1 = (-b + math.sqrt(discriminant)) / (2*a)
-#         x2 = (-b - math.sqrt(discriminant)) / (2*a)
-#         print("The equation has two real roots: x1 =", x1, "and x2 =", x2)
-# except DiscriminantError as e:
-#     print("Error:", e)
+import math
 
 
-# # Напишіть інтерактивний калькулятор. Передбачається, що користувач вводить формулу, що складається з числа,
-# # оператора (як мінімум + і -) та іншого числа, розділених пробілом (наприклад, 1 + 1). Використовуйте str.split()
-# #
-# # a. Якщо вхідні дані не складаються з трьох елементів, генеруйте ексепшн FormulaError.
-# #
-# # b. Спробуйте перетворити перший і третій елемент на float ( float_value = float(str_value)).
-# #    Спіймайте будь-яку ValueError і згенеруйте замість нього FormulaError
-# #
-# # c. Якщо другий елемент не є «+» або «-», киньте ексепшн FormulaError
+class DiscriminantError(Exception):
+    pass
+
+
+a = float(input("Enter the coefficient a: "))
+b = float(input("Enter the coefficient b: "))
+c = float(input("Enter the coefficient c: "))
+
+try:
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant < 0:
+        raise DiscriminantError("Discriminant is negative, cannot take square root of negative number.")
+    elif discriminant == 0:
+        x = -b / (2 * a)
+        print("The equation has one real root: x =", x)
+    else:
+        x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        x2 = (-b - math.sqrt(discriminant)) / (2 * a)
+        print("The equation has two real roots: x1 =", x1, "and x2 =", x2)
+except DiscriminantError as e:
+    print("Error:", e)
+
+
+# Напишіть інтерактивний калькулятор. Передбачається, що користувач вводить формулу, що складається з числа,
+# оператора (як мінімум + і -) та іншого числа, розділених пробілом (наприклад, 1 + 1). Використовуйте str.split()
+#
+# a. Якщо вхідні дані не складаються з трьох елементів, генеруйте ексепшн FormulaError.
+#
+# b. Спробуйте перетворити перший і третій елемент на float ( float_value = float(str_value)).
+#    Спіймайте будь-яку ValueError і згенеруйте замість нього FormulaError
+#
+# c. Якщо другий елемент не є «+» або «-», киньте ексепшн FormulaError
 
 
 class FormulaError(Exception):
