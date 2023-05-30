818d3235a9d64743c2cb48ed107b2f5966a5b906
jenya <132168049+LonelyRave@users.noreply.github.com>
2023-05-30 04:33:45 +0100

    add lesson_12

---
+++ lesson_12/millennium_falcon.py
@@ -0,0 +1,43 @@
+import json
+import urllib.request
+
+
+def get_ship_info():
+    url = "https://swapi.dev/api/starships/?search=Millennium%20Falcon"
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read().decode())
+    ship_info = {}
+
+    if data["count"] > 0:
+        ship = data["results"][0]
+        ship_info["name"] = ship["name"]
+        ship_info["max_speed"] = ship["max_atmosphering_speed"]
+        ship_info["class"] = ship["starship_class"]
+        ship_info["pilots"] = []
+        for pilot_url in ship["pilots"]:
+            pilot = get_pilot_info(pilot_url)
+            ship_info["pilots"].append(pilot)
+    return ship_info
+
+
+def get_pilot_info(pilot_url):
+    response = urllib.request.urlopen(pilot_url)
+    pilot = json.loads(response.read().decode())
+    pilot_info = {}
+    pilot_info["name"] = pilot["name"]
+    pilot_info["height"] = pilot["height"]
+    pilot_info["weight"] = pilot["mass"]
+    pilot_info["homeworld"] = pilot["homeworld"]
+    pilot_info["homeworld_info"] = get_planet_info(pilot["homeworld"])
+    return pilot_info
+
+
+def get_planet_info(planet_url):
+    response = urllib.request.urlopen(planet_url)
+    planet = json.loads(response.read().decode())
+    planet_info = {}
+    planet_info["name"] = planet["name"]
+    planet_info["url"] = planet["url"]
+    return planet_info
+
+# Теперь можно продолжить использовать функцию get_ship_info() и остальной код в вашем main.py
---
+++ lesson_12/quadratic_equation.py
@@ -0,0 +1,16 @@
+import math
+
+
+def solve_quadratic_equation(a, b, c):
+    discriminant = b ** 2 - 4 * a * c
+
+    if discriminant > 0:
+        sqrt_discriminant = math.sqrt(discriminant)
+        x1 = (-b + sqrt_discriminant) / (2 * a)
+        x2 = (-b - sqrt_discriminant) / (2 * a)
+        return x1, x2
+    elif discriminant == 0:
+        x = -b / (2 * a)
+        return x, None
+    else:
+        return None, None
